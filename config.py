19b4e9289c7e9283e912d94ef296dee6c5d440df
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-07-03 03:54:59 +0900

    CHORE: Load environment variables from .env file

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 CHANNEL_ID = 944090089376583711
 SAVE_DIR = "/home/ry2x/Develop/Obsidian-discord/memos"
 IMAGE_SAVE_DIR = "/home/ry2x/Develop/Obsidian-discord/images"
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
