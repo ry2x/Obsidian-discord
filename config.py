4cf971e01d8519084a1811946f5cbcf5d7af60e3
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-07-06 03:59:39 +0900

    ADD: Define NOTES_DIR for notes storage path

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ load_dotenv()
 CHANNEL_ID = 944090089376583711
 SAVE_DIR = "/home/ry2x/Develop/Obsidian-discord/memos"
 IMAGE_SAVE_DIR = "/home/ry2x/Develop/Obsidian-discord/images"
+NOTES_DIR = "/home/ry2x/Develop/Obsidian-discord/notes"
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
