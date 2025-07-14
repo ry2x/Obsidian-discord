32356ea79c94391eef6e9b265c0e2340cf119552
Ry2X <45420571+ry2x@users.noreply.github.com>
2025-07-14 23:48:55 +0900

    FEAT: Update config to use environment variables

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CHANNEL_ID = 794966343350616085
-SAVE_DIR = "/home/ry2x/Develop/Obsidian-discord/memos"
-IMAGE_SAVE_DIR = "/home/ry2x/Develop/Obsidian-discord/images"
-NOTES_DIR = "/home/ry2x/Develop/Obsidian-discord/notes"
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+SAVE_DIR = os.getenv("SAVE_DIR")
+IMAGE_SAVE_DIR = os.getenv("IMAGE_SAVE_DIR")
+NOTES_DIR = os.getenv("NOTES_DIR")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
